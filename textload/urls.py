0c751cccf60d2b5851779a2834e382f4e62b3182
Sharon0320 <sharon0320@gachon.ac.kr>
2024-12-08 11:09:13 +0900

    현재 본문 첫줄만 보내게 설정

--- textload/urls.py
+++ textload/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from textload.views import hello_world
+from . import views
 
 urlpatterns = [
     path('originaltext/', hello_world),
+    path('content/',views.content)
 ]
